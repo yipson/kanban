a214aca55734c3012b21a5d1c75b15fd295724b9
Sebastian Valencia <sebastianvalencia.ceron@gmail.com>
2022-02-03 18:26:03 -0500

    Personalizando paths de los microservicios para ser consultados desde api-gateway

---
+++ api-gateway/src/main/java/com/techm/apigateway/ApiGatewayConfiguration.java
@@ -0,0 +1,33 @@
+package com.techm.apigateway;
+
+import org.springframework.cloud.gateway.route.Route;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.Buildable;
+import org.springframework.cloud.gateway.route.builder.PredicateSpec;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.function.Function;
+
+@Configuration
+public class ApiGatewayConfiguration {
+
+    @Bean
+    public RouteLocator gatewayRouter(RouteLocatorBuilder builder){
+
+        return builder.routes()
+                .route(p -> p.path("/get")
+                        .filters(f -> f
+                                .addRequestHeader("MyHeader", "MyURI")
+                                .addRequestParameter("Param", "MyValue"))
+                        .uri("http://httpbin.org:80"))
+                .route(p -> p.path("/board/**")
+                        .uri("lb://board-service"))
+                .route(p -> p.path("/task/**")
+                        .uri("lb://tasks-service"))
+                .route(p -> p.path("/user/**")
+                        .uri("lb://users-service"))
+                .build();
+    }
+}
