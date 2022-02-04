36edf96e6e998cba14a1ab0521ee606cf7f8f527
Sebastian Valencia <sebastianvalencia.ceron@gmail.com>
2022-02-03 19:30:34 -0500

    Realizando pruebas con Resilience4j

--- users/src/main/java/com/techm/users/controller/CircuitBreakerController.java
+++ users/src/main/java/com/techm/users/controller/CircuitBreakerController.java
@@ -1,2 +1,27 @@
-package com.techm.users.controller;public class CircuitBreakerController {
+package com.techm.users.controller;
+
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+
+@RestController
+public class CircuitBreakerController {
+
+    private Logger logger =
+                LoggerFactory.getLogger(CircuitBreakerController.class);
+
+    @GetMapping("/sample-api")
+    @Retry(name = "sample-api")
+    public String sampleApi(){
+        logger.info("Sample Api call received");
+        ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dumy-url",
+                String.class);
+
+        return forEntity.getBody();
+    }
 }
