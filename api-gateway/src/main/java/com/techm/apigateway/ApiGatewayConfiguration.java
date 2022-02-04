3336955b82234d003e41efcb0e11871e0049d0de
Sebastian Valencia <sebastianvalencia.ceron@gmail.com>
2022-02-04 12:58:22 -0500

    Hardcodeando el token que requieren los microservicios

--- api-gateway/src/main/java/com/techm/apigateway/ApiGatewayConfiguration.java
+++ api-gateway/src/main/java/com/techm/apigateway/ApiGatewayConfiguration.java
@@ -13,17 +13,33 @@ public class ApiGatewayConfiguration {
     public RouteLocator gatewayRouter(RouteLocatorBuilder builder){
 
         return builder.routes()
-                .route(p -> p.path("/get")
+                .route(p -> p
+                        .path("/get")
                         .filters(f -> f
-                                .addRequestHeader("MyHeader", "MyURI")
+                                .addRequestHeader("MyHeaderRequest", "valueRequest")
+                                .addResponseHeader("MyHeaderResponse", "valueResponse")
                                 .addRequestParameter("Param", "MyValue"))
                         .uri("http://httpbin.org:80"))
+
                 .route(p -> p.path("/board/**")
                         .uri("lb://board-service"))
+
                 .route(p -> p.path("/task/**")
                         .uri("lb://tasks-service"))
-                .route(p -> p.path("/user/**")
+
+                .route(p -> p
+                        .path("/user/**")
+                        .filters(f -> f
+//                                .addRequestHeader("Authorization", TOKEN)
+//                                .addResponseHeader("Authorization", TOKEN)
+                                .addRequestHeader("Authorization", "eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI5IiwiaWF0IjoxNjQzOTkzMzc4LCJzdWIiOiJzYW50aWFnb0BtYWlsLmNvbSIsImlzcyI6Ik1haW4iLCJleHAiOjE2NDQ1OTgxNzh9.6MZCZT3mLbYcuaeUiOsDGym6lanqo7vTy03XANGGPwM")
+                                .addResponseHeader("Authorization", "eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI5IiwiaWF0IjoxNjQzOTkzMzc4LCJzdWIiOiJzYW50aWFnb0BtYWlsLmNvbSIsImlzcyI6Ik1haW4iLCJleHAiOjE2NDQ1OTgxNzh9.6MZCZT3mLbYcuaeUiOsDGym6lanqo7vTy03XANGGPwM"))
                         .uri("lb://users-service"))
+//                .route(p -> p
+//                        .path("/login")
+//                        .uri("lb://users-service/login"))
+
                 .build();
     }
+
 }
